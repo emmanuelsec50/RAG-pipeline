7c23d5cd6944c5ce9f6b20fcf1e6009abcb124a5
emmanuelsec50 <mosesemaemanuel@gmail.com>
2026-07-31 15:10:06 +0300

    updated depended files

--- ragapp/utils.py
+++ ragapp/utils.py
@@ -110,7 +110,7 @@ def print_top_results_and_scores(query: str,
                                                   n_resources_to_return=n_resources_to_return)
 
     
-    pages_and_chunks_save_path_pickle = "./ragapp/pages_and_chunks1.pkl"
+    pages_and_chunks_save_path_pickle = "./ragapp/pages_and_chunks2.pkl"
     with open(pages_and_chunks_save_path_pickle, "rb") as f:
         pages_and_chunks = pickle.load(f)
     
@@ -166,13 +166,13 @@ def ask(query: str,
     # RETRIEVAL
     # Get just the scores and indices of top related results
     # t5 = time.time()
-    embeddings = torch.load('./ragapp/embeddings1.pt')
+    embeddings = torch.load('./ragapp/embeddings2.pt')
     # print(f"[TIMING] torch.load: {time.time() - t5:.2f}s")
     scores, indices = retrieve_relevant_resources(query=query,
                                                   embeddings=embeddings)
 
     # Create a list of context items
-    pages_and_chunks_save_path_pickle = "./ragapp/pages_and_chunks1.pkl"
+    pages_and_chunks_save_path_pickle = "./ragapp/pages_and_chunks2.pkl"
     # t6 = time.time()
     with open(pages_and_chunks_save_path_pickle, "rb") as f:
         pages_and_chunks = pickle.load(f)
